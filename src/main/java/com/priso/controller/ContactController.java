1fda42e3408e3c0957395b71054e58eda1b0f20e
Shivam Sharma <shivamsharma.ade@gmail.com>
2022-04-12 01:54:02 +0530

    binding result formtag

--- src/main/java/com/priso/controller/ContactController.java
+++ src/main/java/com/priso/controller/ContactController.java
@@ -6,11 +6,13 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.priso.model.Contact;
 import com.priso.model.User;
 import com.priso.service.UserService;
 
@@ -63,5 +65,19 @@ public class ContactController {
 		// in the jsp properties can be accessed even when they are private
 		model.addAttribute("id", service.createUser(user));
 		return "success-ModelAttribute";
+	}
+
+	@RequestMapping(path = "/contact-details")
+	public String contactForm() {
+		System.out.println("contact Form Handler BindingResult");
+		return "form-to-check-binding-result";
+	}
+
+	@RequestMapping(path = "/process-form-binding-result", method = RequestMethod.POST)
+	public String handleContactForm(@ModelAttribute("contact") Contact contact, BindingResult bindingResult) {
+		System.out.println("handleContactForm Handler BindingResult");
+		if (bindingResult.hasErrors())
+			return "error-invalid-field-value";
+		return "success-contact-form";
 	}
 }
---
+++ src/main/java/com/priso/model/Contact.java
@@ -0,0 +1,14 @@
+package com.priso.model;
+
+public class Contact {
+	private long number;
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+}

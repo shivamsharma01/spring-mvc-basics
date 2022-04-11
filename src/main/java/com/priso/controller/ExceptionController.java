3adf33c71332b8f3141eb16a3b602a78da840d4b
Shivam Sharma <shivamsharma.ade@gmail.com>
2022-04-12 03:31:04 +0530

    exception handler

---
+++ src/main/java/com/priso/controller/ExceptionController.java
@@ -0,0 +1,49 @@
+package com.priso.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class ExceptionController {
+
+	@RequestMapping("/initial-null")
+	public String initialPage() {
+		String x = null;
+		x.length(); // generating a null pointer exception and it will be handled by
+					// expectionHandlerNull method
+		return "index";
+	}
+
+	@RequestMapping("/initial-index")
+	public String initialPage2() {
+		String[] arr = new String[] {};
+		arr[2] = "";
+		return "index";
+	}
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ NullPointerException.class, NumberFormatException.class })
+	public String exceptionHandlerNull(Model model) {
+		model.addAttribute("errMsg", "Null Pointer Exception");
+		return "null_page";
+	}
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler({ ArrayIndexOutOfBoundsException.class })
+	public String exceptionHandlerIndex(Model model) {
+		model.addAttribute("errMsg", "Array Index Out Of Bound Exception");
+		return "null_page";
+	}
+
+	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({ Exception.class })
+	public String exceptionHandlerGeneric(Model model) {
+		model.addAttribute("errMsg", "An Exception occurred");
+		return "null_page";
+	}
+
+}
